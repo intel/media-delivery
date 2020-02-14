936cb7f924b11d87e65d1cb014c88aaafa93e223
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2020-02-14 12:31:56 -0800

    Improve entypoints configuration
    This refactors entrypoints configuration and simplifies fe things.
    Key changes are:
     * Now user enters container with correct and ready to run environment.
       For example, user added to proper groups and shell is ready from
       environment and groups access perspective
     * Now it is possible to run some commands like ffmpeg thru docker-run
       command line without entering container
    
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- solutions/edge/setup.sh
+++ solutions/edge/setup.sh
@@ -6,7 +6,7 @@
 # adjusting access rights, copying configuration files and executables.
 #
 # Mind 2 key scripts created by this setup:
-#  /usr/bin/demo - shell script which is an entrypoint to the solution
+#  /usr/bin/demo-bash - shell script which is an entrypoint to the solution
 #  /etc/demo.env - script containing solution environment variables to
 #    source in shell scripts
 
@@ -25,7 +25,7 @@ cp nginx.conf /etc/nginx/nginx.conf
 
 # Installing solution scripts
 scripts=" \
-  ../../assets/demo.sh \
+  ../../assets/demo \
   ../../assets/ffmpeg-capture-hls.sh \
   ../../scripts/setup-apt-proxy.sh \
   nginx-rtmp-trigger-streaming.sh"
@@ -34,33 +34,12 @@ for s in $scripts; do
   cp $s $prefix/bin
 done
 
-if_grps_changed=" \
-  \"\$(groups \$(whoami) | awk -F: '{print \$2}' | awk '{\$1=\$1;print}')\" != \
-  \"\$(groups)\""
-
 # Generating entrypoints
 {
-  echo "export PATH=$PREFIX/bin:\$PATH"
+  echo "export DEMO_NAME=Edge"
+  echo "export DEMO_PREFIX=$prefix"
+  echo "export PATH=\$DEMO_PREFIX/bin:\$PATH"
   echo "export LIBVA_DRIVER_NAME=iHD"
-  echo "video_grps=\$(ls -g /dev/dri/renderD* | awk '{print \$3}' | uniq | \
-    awk -vORS=, '{ print }' | sed 's/,$/\n/')"
-  echo "sudo usermod -aG \$video_grps user"
-  echo "sudo usermod -aG \$video_grps www-data"
-  # checking whether groups actually changed to avoid abusing user with the messages
-  echo "if [ $if_grps_changed ]; then"
-  echo "  echo \"We just added 'user' and 'www-data' accounts to some groups. If you\""
-  echo "  echo \"run the shell under one of these accounts - you need to restart it.\""
-  echo "  echo \"This can be done by these commands without leaving your current shell:\""
-  echo "  echo \"  exec sudo su - user\""
-  echo "  echo \"  exec sudo su - www-data\""
-  echo "fi"
 } > /etc/demo.env
 
-{
-  echo "#!/bin/bash"
-  echo "source /etc/demo.env > /dev/null"
-  echo "if [ $if_grps_changed ]; then exec sudo -u user -- "\$0" "\$@"; fi"
-  echo "$prefix/bin/demo.sh \$@"
-} > /usr/bin/demo
-
-chmod a+x /usr/bin/demo
+cp demo-setup /usr/bin/
