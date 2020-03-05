9f1d4171b106d3e1c48fc71f99c6a9ae013692fb
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2020-03-05 11:25:05 -0800

    demo: simplify nginx server monitoring script
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- assets/monitor-nginx-server.sh
+++ assets/monitor-nginx-server.sh
@@ -6,45 +6,18 @@ export _scheduled=$ARTIFACTS/scheduled
 export _done=$ARTIFACTS/done
 
 function watch_server() {
-  echo "nginx server monitor"
-  echo "===================="
-  s=$(cat $_scheduled 2>/dev/null | wc -l)
-  d=$(cat $_done 2>/dev/null | wc -l)
-
-  echo "Total requests: $s"
-
-  echo "Running requests: $(($s-$d))"
+  running=0
+  completed=0
   if [ -f $_scheduled ]; then
-    # printing info for each currently running process
-    while read p; do
-      if ! fgrep $p $_done >/dev/null 2>&1; then
-        name=$(echo "$p" | awk -F: '{print $2}')
-        log=$(echo "$p" | awk -F: '{print $3}')
-
-        fragments=$(ls -1 /var/www/hls/live/$name/stream_0/ 2>/dev/null | wc -l)
-
-        line=$(cat $log | sed 's/\r/\n/' | grep frame= | tail -1)
-        # frame= x fps= xx ...
-        frames=$(echo $line | awk -F'[ |=]+' '{print $2}')
-        fps=$(echo $line | awk -F'[ |=]+' '{print $4}')
-
-        echo | awk \
-          -v name=$name \
-          -v frames=$frames \
-          -v fps=$fps \
-          -v fragments=$fragments \
-          '{print "  " name ": frames=" frames ", fps=" fps ", fragments=" fragments}'
-      fi
-    done <$_scheduled
-  fi
-
-  echo "Completed requests: $d"
-  if [ -f $_done ]; then
-    # printing info for each currently running process
     while read p; do
       name=$(echo "$p" | awk -F: '{print $2}')
       log=$(echo "$p" | awk -F: '{print $3}')
-      status=$(echo "$p" | awk -F: '{print $4}')
+      if [ -f $_done ]; then
+        done_line=$(fgrep $p $_done)
+        if $!; then
+          status=$(echo "$done_line" | awk -F: '{print $4}')
+        fi
+      fi
 
       fragments=$(ls -1 /var/www/hls/live/$name/stream_0/ 2>/dev/null | wc -l)
 
@@ -53,15 +26,31 @@ function watch_server() {
       frames=$(echo $line | awk -F'[ |=]+' '{print $2}')
       fps=$(echo $line | awk -F'[ |=]+' '{print $4}')
 
-      echo | awk \
+      report_line=$(echo | awk \
         -v name=$name \
         -v frames=$frames \
         -v fps=$fps \
         -v fragments=$fragments \
-        -v status=$status \
-        '{print "  " name ": frames=" frames ", fps=" fps ", fragments=" fragments ", status=" status}'
-    done <$_done
+        '{print "  " name ": frames=" frames ", fps=" fps ", fragments=" fragments}')
+      if [ -n "$status" ]; then
+        report_line+=$(echo | awk -v status=$status '{print ", status=" status}')
+        completed=$((++completed))
+        completed_reports+="${report_line}\n"
+      else
+        running=$((++running))
+        running_reports+="${report_line}\n"
+      fi
+    done <$_scheduled
   fi
+
+  echo "nginx server monitor"
+  echo "===================="
+  echo "Logs path: $ARTIFACTS"
+  echo "Total requests: $((running+completed))"
+  echo "Running requests: $running"
+  echo -ne "$running_reports"
+  echo "Completed requests: $completed"
+  echo -ne "$completed_reports"
 
   echo
   echo "CTRL^C to exit monitor and enter shell"
